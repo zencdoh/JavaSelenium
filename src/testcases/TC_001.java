bc6912dfff17ba6eca266af7440f89e0bd97a044
zencdoh <adminhybridit@imsrndlabs.com>
2019-06-03 11:50:31 +0530

    updated
    Set to working state

--- src/testcases/TC_001.java
+++ src/testcases/TC_001.java
@@ -3,29 +3,15 @@ package testcases;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 //updated by bala
-import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 //
-public class TC_001a {
+public class TC_001 {
 
 	@Test
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver");
 		ChromeDriver driver = new ChromeDriver();
-		ChromeOptions options = new ChromeOptions();
-        	options.addArguments("headless");
-        	options.addArguments("window-size=1200x600");
-        	WebDriver driver = new ChromeDriver(options);
-		
-		driver.get("http://www.facebook.com");
+		  driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
 		driver.findElementById("pass").sendKeys("Hello");
 		driver.quit();
