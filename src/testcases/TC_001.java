12dcd15951af4760a5a14130e1dc24a93a7fda52
zencdoh <adminhybridit@imsrndlabs.com>
2019-06-03 11:39:06 +0530

    updation
    bala-updated headless chrome mode

--- src/testcases/TC_001.java
+++ src/testcases/TC_001.java
@@ -2,13 +2,24 @@ package testcases;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+//updated by bala
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//
 public class TC_001 {
 
 	@Test
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver");
+		chromeOptions.addArguments("--headless");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
