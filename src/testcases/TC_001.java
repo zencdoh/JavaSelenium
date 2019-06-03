265b7ee2c4036d4345a0ee2eafd6f88f9ced9807
zencdoh <adminhybridit@imsrndlabs.com>
2019-06-03 14:21:26 +0530

    updated
    fix compilation

--- src/testcases/TC_001.java
+++ src/testcases/TC_001.java
@@ -2,11 +2,7 @@ package testcases;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import org.openqa.selenium.WebDriver;
-iimport org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.Test;
-//updated by bala
-//
+
 public class TC_001 {
 
 	@Test
@@ -14,15 +10,11 @@ public class TC_001 {
 	{
 		System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver");
 		ChromeDriver driver = new ChromeDriver();
-		ChromeOptions option=new ChromeOptions();
-		option.setHeadless(true);		
-		WebDriver driver=new ChromeDriver(option);
 		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
 		driver.findElementById("pass").sendKeys("Hello");
 		driver.quit();
 	}
-	
 	
 	
 }
