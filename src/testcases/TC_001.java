a024e6056c42f6a8bfc622362db39078f6f41d7d
zencdoh <adminhybridit@imsrndlabs.com>
2019-06-03 14:16:15 +0530

    updated
    chrome withe headless mode

--- src/testcases/TC_001.java
+++ src/testcases/TC_001.java
@@ -2,6 +2,9 @@ package testcases;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+iimport org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 //updated by bala
 //
 public class TC_001 {
@@ -11,11 +14,18 @@ public class TC_001 {
 	{
 		System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver");
 		ChromeDriver driver = new ChromeDriver();
-		  driver.get("http://www.facebook.com");
+		ChromeOptions option=new ChromeOptions();
+		option.setHeadless(true);		
+		WebDriver driver=new ChromeDriver(option);
+		driver.get("http://www.facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
 		driver.findElementById("pass").sendKeys("Hello");
 		driver.quit();
 	}
 	
 	
+	
 }
+
+
+
